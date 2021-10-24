2d45cd69a952dc0873d965721893c9ed284b94d5
emanuele-moricci <emanuele.moricci@gmail.com>
2021-10-24 21:43:50 +0200

    Added the authentication shielding method to verify a logged-in user

--- back-end/federation-auth/src/graphql/schema/Models/User/queries/GetAllUsers.ts
+++ back-end/federation-auth/src/graphql/schema/Models/User/queries/GetAllUsers.ts
@@ -11,13 +11,15 @@ import { User } from '@prisma/client';
 import UserType from '@src/graphql/schema/Models/User/User';
 
 import { getAllUsers } from '@src/services/userService';
+import { shieldedMethod } from '@src/graphql/schema/Utils/JWTToken';
 
 export const getAllUsersResolver: GraphQLFieldResolver<
   unknown,
   IApolloServerContext
-> = async (_source, _args, { prismaContext }, _info): Promise<User[]> => {
-  return await getAllUsers();
-};
+> = async (_source, _args, { token, user }, _info): Promise<User[]> =>
+  shieldedMethod(token, async () => {
+    return await getAllUsers();
+  });
 
 const getAllUsersQuery: GraphQLFieldConfig<unknown, IApolloServerContext> = {
   description: 'Get all Users query',
--- back-end/federation-auth/src/graphql/schema/Utils/JWTToken.ts
+++ back-end/federation-auth/src/graphql/schema/Utils/JWTToken.ts
@@ -1,7 +1,13 @@
 import jwt from 'jsonwebtoken';
 
 export const jwtSign = (userId: number): string =>
-  jwt.sign({ userId }, process.env.AUTH_JWT_SECRET ?? '');
+  jwt.sign({ userId }, process.env.AUTH_JWT_SECRET ?? '', { expiresIn: '3d' });
 
 export const jwtVerify = (token: string): string | object =>
-  jwt.verify(token, process.env.AUTH_JWT_SECRET ?? '');
+  jwt.verify(token, process.env.AUTH_JWT_SECRET ?? '', {});
+
+export const shieldedMethod = (token: string | null, callback: () => any) => {
+  jwtVerify(token ?? '');
+
+  return callback();
+};
