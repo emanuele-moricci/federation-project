b421594ed9026db6e641cf14150cafbfcc36f30a
emanuele-moricci <emanuele.moricci@gmail.com>
2021-10-28 22:15:16 +0200

    squash

--- back-end/services/federation-auth/src/graphql/schema/Models/User/User.resolver.ts
+++ back-end/services/federation-auth/src/graphql/schema/Models/User/User.resolver.ts
@@ -8,25 +8,31 @@ import {
 import { getSecurityByUserId } from '@src/services/securityService';
 import { Language } from '@src/graphql/generated/graphql';
 
-import { IUserRef, ILanguageRef } from '@src/../../utils/types/refs';
-import authGuard from '@src/../../utils/permissions';
+interface IUserRef {
+  __typename: 'User';
+  userId: string;
+  languageId: string;
+}
+
+interface ILanguageRef {
+  __typename: 'Language';
+  languageId: string;
+}
 
 const resolver = {
   Query: {
-    User: async (_source, args, context, _info): Promise<User[]> => {
-      return authGuard(context) ? getAllUsers(args) : [];
+    User: async (_source, args, _context, _info): Promise<User[]> => {
+      return await getAllUsers(args);
     },
-    me: async (_source, _args, context, _info): Promise<User | null> => {
-      return authGuard(context)
-        ? getUserById(context?.userData?.userId ?? -1)
-        : null;
+    me: async (_source, _args, { userData }, _info): Promise<User | null> => {
+      return await getUserById(userData?.userId ?? -1);
     },
   },
   User: {
     __resolveReference: async ({ userId }: IUserRef): Promise<User | null> => {
       return getUserById(parseInt(userId));
     },
-    security: async ({ userId }: any): Promise<Security | null> => {
+    security: async ({ userId }: any): Promise<Security | any> => {
       return getSecurityByUserId(userId);
     },
     language: ({ languageId }: IUserRef): Language => ({
@@ -36,14 +42,8 @@ const resolver = {
   },
   // EXTENSIONS
   Language: {
-    users: async (
-      { languageId }: ILanguageRef,
-      _args,
-      context
-    ): Promise<User[]> => {
-      return authGuard(context)
-        ? getUsersByLanguageId(parseInt(languageId))
-        : [];
+    users: async ({ languageId }: ILanguageRef): Promise<User[]> => {
+      return getUsersByLanguageId(parseInt(languageId));
     },
   },
 };
