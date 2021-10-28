21bdee43efd736760795d712759adaa037a8d1fe
emanuele-moricci <emanuele.moricci@gmail.com>
2021-10-28 12:39:19 +0200

    Fixed security not resolving the user type

--- back-end/services/federation-auth/src/graphql/schema/Models/Security/Security.resolver.ts
+++ back-end/services/federation-auth/src/graphql/schema/Models/Security/Security.resolver.ts
@@ -1,6 +1,7 @@
-import { Security } from '@prisma/client';
+import { Security, User } from '@prisma/client';
 
 import { getSecurityById } from '@src/services/securityService';
+import { getUserById } from '@src/services/userService';
 
 import { ISecurityRef } from '@src/../../utils/types/refs';
 
@@ -10,6 +11,9 @@ const resolver = {
       securityId,
     }: ISecurityRef): Promise<Security | null> => {
       return getSecurityById(parseInt(securityId));
+    },
+    user: async ({ userId }: any): Promise<User | null> => {
+      return getUserById(userId);
     },
   },
 };
