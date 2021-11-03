20e6be8c4b4b777f027d4510d1d4e0a169662917
emanuele-moricci <emanuele.moricci@gmail.com>
2021-11-03 16:06:34 +0100

    Moved all of the route shielders under 'federation-utils' + updated the 'federation-generator'

--- _utilities/src/index.ts
+++ _utilities/src/index.ts
@@ -2,6 +2,7 @@ import resolvers from './resolvers';
 
 import { cryptObject, decryptObject } from './security/Hashing';
 import { verifyToken, signToken } from './security/JWT';
+import { isAuthenticated, isAdmin, alwaysAllow } from './security/Shielding';
 
 import { PaginationAndSearchArgs } from './types/QueryArgs';
 
@@ -13,4 +14,7 @@ export {
   resolvers,
   verifyToken,
   signToken,
+  isAuthenticated,
+  isAdmin,
+  alwaysAllow,
 };
--- back-end/services/federation-auth/src/graphql/schema/permissions.ts
+++ back-end/services/federation-auth/src/graphql/schema/permissions.ts
@@ -1,22 +1,5 @@
-import { shield, rule, not } from 'graphql-shield';
-
-const isAuthenticated = rule({ cache: 'contextual' })(
-  async (_parent, _args, { userData }, _info) => {
-    return userData && Object.keys(userData).length > 0
-      ? userData.userId !== null
-      : false;
-  }
-);
-
-const isAdmin = rule({ cache: 'contextual' })(
-  async (_parent, _args, { userData }, _info) => {
-    return userData.role === 'ADMIN';
-  }
-);
-
-const alwaysAllow = rule({ cache: 'contextual' })(
-  async (_parent, _args, _context, _info) => true
-);
+import { shield, not } from 'graphql-shield';
+import { isAuthenticated } from 'federation-utils';
 
 const permissions = shield({
   Query: {
