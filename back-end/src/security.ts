9778651d01a51e86f7684e38181ae26f5a57613a
emanuele-moricci <emanuele.moricci@gmail.com>
2021-11-03 15:10:50 +0100

    Added url encoding and parameter pollution shielding

--- back-end/src/security.ts
+++ back-end/src/security.ts
@@ -1,6 +1,11 @@
+import express from 'express';
+import hpp from 'hpp';
 import toobusy from 'toobusy-js';
 
 const secureApp = app => {
+  app.use(express.urlencoded({ extended: false }));
+  app.use(hpp());
+
   app.use(function (_, res, next) {
     if (toobusy()) {
       res.status(503).send("🚫 I'm busy right now, sorry.");
