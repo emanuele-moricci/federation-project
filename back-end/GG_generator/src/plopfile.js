f763e474b3522cdee66133b1d5319a936a7e275c
emanuele-moricci <emanuele.moricci@gmail.com>
2021-11-15 13:06:03 +0100

    Added the new Extensions generator from GalactaGraph

--- back-end/GG_generator/src/plopfile.js
+++ back-end/GG_generator/src/plopfile.js
@@ -2,11 +2,13 @@ module.exports = function (plop) {
   // GENERATORS
   let serviceGenerator = require("./Back-End/Service/index.js");
   let modelGenerator = require("./Back-End/Model/index.js");
+  let extensionGenerator = require("./Back-End/Extension/index.js");
   let queryGenerator = require("./Back-End/Query/index.js");
   let mutationGenerator = require("./Back-End/Mutation/index.js");
 
   plop.setGenerator("service", serviceGenerator);
   plop.setGenerator("model", modelGenerator);
+  plop.setGenerator("extension", extensionGenerator);
   plop.setGenerator("query", queryGenerator);
   plop.setGenerator("mutation", mutationGenerator);
 
@@ -39,6 +41,14 @@ module.exports = function (plop) {
     const pluralize = require("pluralize");
     const pluarlized = pluralize(text);
     return pluarlized.toUpperCase();
+  });
+
+  plop.setHelper("eq", function (arg1, arg2) {
+    return arg1 == arg2;
+  });
+
+  plop.setHelper("or", function () {
+    return Array.prototype.slice.call(arguments, 0, -1).some(Boolean);
   });
 
   // CUSTOM ACTIONS
